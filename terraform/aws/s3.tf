93b329a2a600f0cf05afb9bd631973be29b3a86c
jonnyCodev <42693227+jonnyCodev@users.noreply.github.com>
2021-08-03 15:39:45 +0300

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -6,6 +6,8 @@ resource "aws_s3_bucket" "data" {
   bucket        = "${local.resource_prefix.value}-data"
   acl           = "public-read"
   force_destroy = true
+  
+  
   tags = merge({
     Name        = "${local.resource_prefix.value}-data"
     Environment = local.resource_prefix.value
